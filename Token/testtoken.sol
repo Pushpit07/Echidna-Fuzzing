ee358b4e57f44c17dcf455db995fc74824379d2c
Pushpit <pushpit07@gmail.com>
2021-12-08 14:52:46 +0530

    first commit

---
+++ Token/testtoken.sol
@@ -0,0 +1,14 @@
+// contracts/EchidnaTesting.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "token.sol";
+
+contract TestToken is Token {
+    // Echidna needs a constructor without argument. If your contract needs a specific initialization, you need to do it in the constructor.
+    constructor() {}
+
+    function echidna_balance_under_1000() public view returns (bool) {
+        return balances[msg.sender] <= 1000;
+    }
+}
---
+++ Token/token.sol
@@ -0,0 +1,20 @@
+// contracts/Musixverse.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    mapping(address => uint256) public balances;
+
+    function airdrop() public {
+        balances[msg.sender] = 1000;
+    }
+
+    function consume() public {
+        require(balances[msg.sender] > 0);
+        balances[msg.sender] -= 1;
+    }
+
+    function backdoor() public {
+        balances[msg.sender] += 1;
+    }
+}
