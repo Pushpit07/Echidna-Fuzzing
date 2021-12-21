caa8d7b95d543ebcb5da704ad50b92f02bd28109
Pushpit <pushpit07@gmail.com>
2021-12-21 13:52:07 +0530

    Assert
    Signed-off-by: Pushpit <pushpit07@gmail.com>

---
+++ Inc/assert.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Incrementor {
+    uint256 private counter = 2**200;
+
+    function inc(uint256 val) public returns (uint256) {
+        uint256 tmp = counter;
+        counter += val;
+        assert(tmp <= counter);
+        return (counter - tmp);
+    }
+}
